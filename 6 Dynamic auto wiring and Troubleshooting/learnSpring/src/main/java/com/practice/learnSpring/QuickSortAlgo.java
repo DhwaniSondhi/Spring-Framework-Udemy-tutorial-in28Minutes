1bf162371e60b04fe271a2c1ee5130ff5bd43c23
DSondhi <dhwanisondhi3@gmail.com>
2020-04-13 01:00:55 -0400

    Dynamic auto wiring and Troubleshooting Lecture

---
+++ 6 Dynamic auto wiring and Troubleshooting/learnSpring/src/main/java/com/practice/learnSpring/QuickSortAlgo.java
@@ -0,0 +1,11 @@
+package com.practice.learnSpring;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class QuickSortAlgo implements SortAlgo{
+
+	public int[] sort(int[] numbers) {
+		return numbers;
+	}
+}
