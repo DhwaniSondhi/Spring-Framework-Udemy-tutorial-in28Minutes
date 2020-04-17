1330eda9f469e42d98294d2d10e43d0fbe63b1f3
DSondhi <dhwanisondhi3@gmail.com>
2020-04-17 01:22:02 -0400

    14 Lifecycle of a Bean Lecture

---
+++ 14 Lifecycle of a Bean/learnSpring/src/main/java/com/practice/learnSpring/springin5Steps/LearnSpringComponentScanApplication.java
@@ -0,0 +1,27 @@
+package com.practice.learnSpring.springin5Steps;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.practice.learnSpring.componentScan.ComponentDAO;
+
+@SpringBootApplication
+@ComponentScan("com.practice.learnSpring.componentScan")
+public class LearnSpringComponentScanApplication {
+
+	private static Logger LOGGER=LoggerFactory.getLogger(LearnSpringComponentScanApplication.class); 
+	public static void main(String[] args) {
+		ApplicationContext context=SpringApplication.run(LearnSpringComponentScanApplication.class, args);
+		ComponentDAO psDao=(ComponentDAO)context.getBean(ComponentDAO.class);
+		LOGGER.info("{}", psDao);
+		
+	}
+	
+
+}
