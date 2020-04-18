62194dc6a15e04a7c780532a43d79205f2111d5b
DSondhi <dhwanisondhi3@gmail.com>
2020-04-18 03:24:40 -0400

    17 Fixing Stuff Lecture

---
+++ 17 Fixing Minor stuff/learnSpring/src/main/java/com/practice/learnSpring/springin5Steps/CDI/CDIPracDao.java
@@ -0,0 +1,11 @@
+package com.practice.learnSpring.springin5Steps.CDI;
+
+import javax.inject.Named;
+
+import org.springframework.stereotype.Component;
+
+@Named
+public class CDIPracDao {
+
+	public CDIPracDao() {}
+}
---
+++ 17 Fixing Minor stuff/learnSpring/src/main/java/com/practice/learnSpring/springin5Steps/scope/PersonDAO.java
@@ -0,0 +1,24 @@
+package com.practice.learnSpring.springin5Steps.scope;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PersonDAO {
+
+	@Autowired
+	
+	JdbcConnection jdbcconnect;
+
+	public JdbcConnection getJdbcconnect() {
+		return jdbcconnect;
+	}
+
+	public void setJdbcconnect(JdbcConnection jdbcconnect) {
+		this.jdbcconnect = jdbcconnect;
+	}
+	
+}
