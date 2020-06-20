40517cd69514bf090da1fdfcd07a4aa80599ef5b
DSondhi <dhwanisondhi3@gmail.com>
2020-06-20 01:05:45 -0400

    Adding Lectures 39-41

---
+++ 39 Setting Up a Project with JDBC, JPA, H2 and Web dependencies/DatabaseDemo/src/test/java/org/prac/jpa/JPADemo/DatabaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prac.jpa.JPADemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
